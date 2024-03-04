cb11bb36209b299d2b3fc1a186812abfd2b5beaa
Dmokrite1 <hg_greg@proton.me>
2024-03-04 12:30:28 +0100

    test paste with jest
    Signed-off-by: Dmokrite1 <hg_greg@proton.me>

--- API-BOT/Tests/database.test.ts
+++ API-BOT/Tests/database.test.ts
@@ -7,5 +7,6 @@ it('fetch users', async () => {
         path: 'development.env'
     });
     await DatabaseConnection.init();
-    const user = await getUserById('1')
+    const user = await getUserById('614e826d-a894-4e2b-adef-d4c8cde0b006')
+    expect(user).toHaveProperty('firstName', 'Seb')
 })
